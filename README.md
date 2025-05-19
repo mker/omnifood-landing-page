7c6401b4a3ea56895015f5f6175c21eb2a57175f
mker <krmichael25@gmail.com>
2025-05-19 17:23:30 +0600

    Add readme, preview images and small layout fixes

---
+++ README.md
@@ -0,0 +1,18 @@
+# Landing page for a food subscription service
+
+[Omnifood live demo](https://omnifood-mker.netlify.app/)
+
+Omnifood landing page
+
+<img width='1000' alt='omnifood-preview' src='./screenshots/omnifood-hero-preview.png'>
+
+## Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- CSS Grid
+- A bit of JS for nav and scrolling, Intersection Observer API
+
+## Credits
+
+This landing page was built as part of [Build Responsive websites course](https://www.udemy.com/course/design-and-develop-a-killer-website-with-html5-and-css3/)
--- js/script.js
+++ js/script.js
@@ -1,72 +1,67 @@
 ///////////////////////////////////////////////////////////
 // Set current year
-const yearEl = document.querySelector('.year');
+const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-const btnNavEl = document.querySelector('.btn-mobile-nav');
-const headerEl = document.querySelector('.header');
-
-btnNavEl.addEventListener('click', () => {
-  headerEl.classList.toggle('nav-open');
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
 });
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
-
-const allLinks = document.querySelectorAll('a:link');
-console.log(allLinks);
+const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
-  link.addEventListener('click', function (e) {
+  link.addEventListener("click", function (e) {
     e.preventDefault();
-    const href = link.getAttribute('href');
+    const href = link.getAttribute("href");
 
     // Scroll back to top
-    if (href === '#')
+    if (href === "#")
       window.scrollTo({
         top: 0,
-        behavior: 'smooth',
+        behavior: "smooth",
       });
 
     // Scroll to other links
-    if (href !== '#' && href.startsWith('#')) {
+    if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: 'smooth' });
+      sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
     // Close mobile navigation
-    if (link.classList.contains('main-nav-link'))
-      headerEl.classList.toggle('nav-open');
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
   });
 });
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
-
-const sectionHeroEl = document.querySelector('.section-hero');
+const sectionHeroEl = document.querySelector(".section-hero");
 
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
 
     if (ent.isIntersecting === false) {
-      document.body.classList.add('sticky');
+      document.body.classList.add("sticky");
     }
 
     if (ent.isIntersecting === true) {
-      document.body.classList.remove('sticky');
+      document.body.classList.remove("sticky");
     }
   },
   {
     // In the viewport
     root: null,
     threshold: 0,
-    rootMargin: '-80px',
-  },
+    rootMargin: "-80px",
+  }
 );
 obs.observe(sectionHeroEl);
